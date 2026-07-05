b48c9b1947542c683c5dc8c024c167da852f000b
ElvisEGP <elvis.egp@outlook.com>
2026-07-05 20:31:43 +0200

    Fixbug files .razor, css, buttons, field search removed because is not work

--- obj/Debug/net10.0/RapportinoServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/RapportinoServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RapportinoServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e288137c4ae99f23c5ee2b3790474b7104ab98c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe05c26b7824b3bf1349849efb36f3a58bdb11b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RapportinoServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RapportinoServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
